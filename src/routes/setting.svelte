e1b7484bad48e3bb54dd5cf6b646cb112ccfac1e
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-06-05 05:20:50 +0700

    quickfix: validate :-1:

--- src/routes/setting.svelte
+++ src/routes/setting.svelte
@@ -6,7 +6,7 @@
 		if (res.ok) {
 			return {
 				props: {
-					listproducts : products,
+					listproducts: products
 				}
 			};
 		}
@@ -20,70 +20,72 @@
 
 <script>
 	export let listproducts;
-  let edit = false;
-  let title = "";
-  $: title = edit ? `Edit Product - ${modalForm.title}` : "Add Product";
-  let modalForm = {
-    title: '',
-    desc: '',
-    img: '',
-    price: 0,
-    type: '',
-  };
-  let selectProduct = {};
-  let selectItem = (item) => {
-    selectProduct = item;
-  };
-  let loadItem = (item) => {
-    modalForm.title = item.title;
-    modalForm.desc = item.desc;
-    modalForm.img = item.img;
-    modalForm.price = item.price;
-    modalForm.type = item.type;
-    selectItem(item);
-    edit = true;
-    modal = true;
-  };
-  let validateForm = () => {
-    if (modalForm.title.trim().length < 1) {
-      alert('Title is required');
-      return false;
-    }
-    if (modalForm.desc.trim().length < 1) {
-      alert('Description is required');
-      return false;
-    }
-    if (new RegExp('^(http|https)://').test(modalForm.img) === false || modalForm.img.trim().length < 1) {
-      alert('Image is required or invalid url');
-      return false;
-    }
-    if (isNaN(modalForm.price)) {
-      alert('Price is Not a number');
-      return false;
-    }
-  }
-  let cleanItem = () => {
-    modalForm.title = '';
-    modalForm.desc = '';
-    modalForm.img = '';
-    modalForm.price = 0;
-    modalForm.type = 'vegatable';
-    edit = false;
-  };
+	let edit = false;
+	let title = '';
+	$: title = edit ? `Edit Product - ${modalForm.title}` : 'Add Product';
+	let modalForm = {
+		title: '',
+		desc: '',
+		img: '',
+		price: 0,
+		type: ''
+	};
+	let selectProduct = {};
+	let selectItem = (item) => {
+		selectProduct = item;
+	};
+	let loadItem = (item) => {
+		modalForm.title = item.title;
+		modalForm.desc = item.desc;
+		modalForm.img = item.img;
+		modalForm.price = item.price;
+		modalForm.type = item.type;
+		selectItem(item);
+		edit = true;
+		modal = true;
+	};
+	let validateForm = () => {
+		if (modalForm.title.trim().length < 1) {
+			alert('Title is required');
+			return false;
+		}
+		if (modalForm.desc.trim().length < 1) {
+			alert('Description is required');
+			return false;
+		}
+		if (
+			new RegExp('^(http|https)://').test(modalForm.img) === false ||
+			modalForm.img.trim().length < 1
+		) {
+			alert('Image is required or invalid url');
+			return false;
+		}
+		if (isNaN(modalForm.price)) {
+			alert('Price is Not a number');
+			return false;
+		}
+    return true;
+	};
+	let cleanItem = () => {
+		modalForm.title = '';
+		modalForm.desc = '';
+		modalForm.img = '';
+		modalForm.price = 0;
+		modalForm.type = 'vegatable';
+		edit = false;
+	};
 
-  let reload = async () => {
-    const res = await fetch('/api/products.json');
-    const { products } = await res.json();
+	let reload = async () => {
+		const res = await fetch('/api/products.json');
+		const { products } = await res.json();
 
-    if (res.ok) {
-      listproducts = products;
-    }
-  };
-  let modal = false;
-  let saveItem = async () => {
-    if (!validateForm()) { 
-      return;
-     }    
+		if (res.ok) {
+			listproducts = products;
+		}
+	};
+	let modal = false;
+	let saveItem = async () => {
+		if (validateForm()) {
       if (!edit) {
         const newProduct = await fetch('/api/products.json', {
           method: 'POST',
@@ -96,7 +98,7 @@
           modal = false;
           await reload();
         }
-      } else {        
+      } else {
         const updateProduct = await fetch(`/api/products/${selectProduct._id}.json`, {
           method: 'PATCH',
           headers: {
@@ -109,18 +111,19 @@
           await reload();
         }
       }
-  }
-  let delItem = async (item) => {
-    const delProduct = await fetch(`/api/products/${item._id}.json`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    if (delProduct) {
-      await reload();
-    }
-  }
+		}
+	};
+	let delItem = async (item) => {
+		const delProduct = await fetch(`/api/products/${item._id}.json`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		if (delProduct) {
+			await reload();
+		}
+	};
 	let password = 'root';
 	let form = {
 		password: null
@@ -134,7 +137,7 @@
 </script>
 
 <svelte:head>
-  <title>Chanta Shop - Setting</title>
+	<title>Chanta Shop - Setting</title>
 </svelte:head>
 
 <div class="container flex justify-center items-center mx-auto h-full">
@@ -163,7 +166,15 @@
 			<table class="table w-full table-zebra">
 				<thead class="sticky top-0">
 					<tr>
-						<th class="flex items-center justify-center "><label class="btn btn-sm modal-button" for="editModal" on:click={() => {cleanItem()}}>+</label></th>
+						<th class="flex items-center justify-center "
+							><label
+								class="btn btn-sm modal-button"
+								for="editModal"
+								on:click={() => {
+									cleanItem();
+								}}>+</label
+							></th
+						>
 						<th class="text-center">Title</th>
 						<th class="text-center">Description</th>
 						<th class="text-center">Price</th>
@@ -180,8 +191,20 @@
 							<td class="text-center">{product.price}</td>
 							<td class="text-center">{product.type}</td>
 							<td class="flex items-center justify-center">
-								<label for="" on:click={() => { loadItem(product) }} class="btn btn-sm modal-button">EDIT</label>
-								<label for="deleteModal" class="btn btn-sm modal-botton" on:click={() => {selectItem(product)}}>DEL</label>
+								<label
+									for=""
+									on:click={() => {
+										loadItem(product);
+									}}
+									class="btn btn-sm modal-button">EDIT</label
+								>
+								<label
+									for="deleteModal"
+									class="btn btn-sm modal-botton"
+									on:click={() => {
+										selectItem(product);
+									}}>DEL</label
+								>
 							</td>
 						</tr>
 					{/each}
@@ -197,24 +220,49 @@
 		<label for="editModal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
 		<h3 class="text-lg font-bold">{title}</h3>
 		<div class="modal-body">
-      <p class="mt-2 pl-2">Title</p>
-      <input type="text" placeholder="Type here" bind:value={modalForm.title} class="input input-bordered w-full max-w mb-2" />
-      <p class="mt-2 pl-2">Description</p>
-      <input type="text" placeholder="Type here" bind:value={modalForm.desc} class="input input-bordered w-full max-w mb-2" />
-      <p class="mt-2 pl-2">Price</p>
-      <input type="text" placeholder="Type here" bind:value={modalForm.price} class="input input-bordered w-full max-w mb-2" />
-      <p class="mt-2 pl-2">Type</p>
-      <select class="select select-bordered w-full" bind:value={modalForm.type}>
-        <option>vegatable</option>
-        <option>fruit</option>
-        <option>bakery</option>
-        <option>spices</option>
-      </select>
-      <p class="mt-2 pl-2">Img URL</p>
-      <input type="text" placeholder="Type here" bind:value={modalForm.img} class="input input-bordered w-full max-w mb-2" />
+			<p class="mt-2 pl-2">Title</p>
+			<input
+				type="text"
+				placeholder="Type here"
+				bind:value={modalForm.title}
+				class="input input-bordered w-full max-w mb-2"
+			/>
+			<p class="mt-2 pl-2">Description</p>
+			<input
+				type="text"
+				placeholder="Type here"
+				bind:value={modalForm.desc}
+				class="input input-bordered w-full max-w mb-2"
+			/>
+			<p class="mt-2 pl-2">Price</p>
+			<input
+				type="text"
+				placeholder="Type here"
+				bind:value={modalForm.price}
+				class="input input-bordered w-full max-w mb-2"
+			/>
+			<p class="mt-2 pl-2">Type</p>
+			<select class="select select-bordered w-full" bind:value={modalForm.type}>
+				<option>vegatable</option>
+				<option>fruit</option>
+				<option>bakery</option>
+				<option>spices</option>
+			</select>
+			<p class="mt-2 pl-2">Img URL</p>
+			<input
+				type="text"
+				placeholder="Type here"
+				bind:value={modalForm.img}
+				class="input input-bordered w-full max-w mb-2"
+			/>
 		</div>
 		<div class="modal-action">
-			<button class="btn" on:click={() => {saveItem()}}>Save</button>
+			<button
+				class="btn"
+				on:click={() => {
+					saveItem();
+				}}>Save</button
+			>
 		</div>
 	</label>
 </label>
@@ -225,10 +273,19 @@
 		<label for="deleteModal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
 		<h3 class="text-lg font-bold">Confirm</h3>
 		<div class="modal-body">
-      <p class="mt-2 pl-2">Do you want to Delete <span class="text-error font-bold">{selectProduct.title}</span> ?</p>
-		<div class="modal-action">
-			<label for="deleteModal" class="btn btn-success">NO</label>
-			<label for="deleteModal" class="btn btn-error" on:click={() => {delItem(selectProduct)}}>YES</label>
-		</div>
-	</label>
+			<p class="mt-2 pl-2">
+				Do you want to Delete <span class="text-error font-bold">{selectProduct.title}</span> ?
+			</p>
+			<div class="modal-action">
+				<label for="deleteModal" class="btn btn-success">NO</label>
+				<label
+					for="deleteModal"
+					class="btn btn-error"
+					on:click={() => {
+						delItem(selectProduct);
+					}}>YES</label
+				>
+			</div>
+		</div></label
+	>
 </label>
